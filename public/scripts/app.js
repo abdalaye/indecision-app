0064a126f6c893f17f22293fa4f153a4e2859fc6
abdalaye <abdalayesoum237@gmail.com>
2020-06-09 12:34:23 +0000

    First commit

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var template = React.createElement(
+  'p',
+  null,
+  'Hello salut'
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,5 @@
+var template = <p>Hello salut</p>;
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot)
