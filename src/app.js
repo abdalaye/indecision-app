1ca1c5333314c6ed580ac55148aec027febbcb11
abdalaye <abdalayesoum237@gmail.com>
2020-06-11 18:22:45 +0000

    Complete Basic components

--- src/app.js
+++ src/app.js
@@ -1,12 +1,50 @@
 class Header extends React.Component {
     render() {
-        return <p>This is From Header</p>;
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
+    }
+}
+
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>>What should I do ?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                Options Component here
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                AddOption component here
+            </div>
+        );
     }
 }
 
 const jsx = (
     <div>
         <Header />
+        <Action />
+        <Options />
+        <AddOption />
     </div>
 );
 
