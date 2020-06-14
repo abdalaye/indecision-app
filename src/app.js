241286477ae95619e4cea587005c67e74029cfc7
abdalaye <abdalayesoum237@gmail.com>
2020-06-14 17:49:12 +0000

    Class properties pour supprimer les bind dans les constructor

--- src/app.js
+++ src/app.js
@@ -3,3 +3,35 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
+
+//IMPORTANCE DE transform-class-properties
+class oldSyntax {
+    constructor() {
+        this.name= 'OldSyntaxe';
+        this.getGretting = this.getGretting.bind(this)
+    }
+
+    getGretting() {
+        return `Hello my name is ${this.name}`;
+    }
+}
+
+const old = new oldSyntax();
+
+console.log(old.name);
+const getGretting = old.getGretting;
+console.log(getGretting());
+
+class newSyntax {
+    name = "New syntaxe";
+
+    getGretting = () => {
+        return `Hello my name is ${this.name}`;
+    }
+}
+
+const newsyn= new newSyntax();
+
+console.log(newsyn.name);
+const newgetGretting = newsyn.getGretting;
+console.log(newgetGretting());
