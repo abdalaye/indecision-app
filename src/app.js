84c9c9023947d75e65d5c7e7e4c590463b96a57e
abdalaye <abdalayesoum237@gmail.com>
2020-06-14 18:05:12 +0000

    Rollback exemple props.children

--- src/app.js
+++ src/app.js
@@ -2,24 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-const Layout = (props) => {
-    return (
-        <div>
-            <p>Header</p>
-            {props.children}
-            <p>Footer</p>
-        </div>
-    );
-}
-
-const template = (
-    <div>
-        <h1>Hello</h1>
-    </div>
-);
-
-ReactDOM.render((
-    <Layout>
-        {template}
-    </Layout>
-), document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
